a84447579ac5bdab239590d113b6e6ccfc6d2116
Rishon73 <sshiff@gmail.com>
2018-10-08 17:09:58 -0400

    applying MCUtil changes

--- src/main/java/com/mf/LeanFtTest.java
+++ src/main/java/com/mf/LeanFtTest.java
@@ -7,16 +7,17 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.hp.lft.sdk.*;
 import com.hp.lft.sdk.mobile.*;
+import com.mf.utils.*;
 
 import unittesting.*;
 
 public class LeanFtTest extends UnitTestClassBase {
     private int counter = 0;
-    private String[] names = {"sshiff", "Shahar2"};
-    private String[] passwords = {"5954194bd5c6399c6cd6ceb9978eb34043372b53e941331", "5954194bd5c6399c6cd6ceb9978eb34043372b53e941331"};
+    private String[] names = {"Shahar", "Shahar2"};
+    private String[] passwords = {"460d4691b2f164b933e1476fa1", "5954194bd5c6399c6cd6ceb9978eb34043372b53e941331"};
     private String OS_TYPE = "Android";
     private String currentDevice;
-    private MCUtils mcutils;
+    private LabDevice mcUtils;
     private AppModelAOS_Android appModel;
 
     @BeforeClass
@@ -29,10 +30,11 @@ public class LeanFtTest extends UnitTestClassBase {
 
     @BeforeMethod
     public void beforeMethod() throws Exception {
-        mcutils = new MCUtils();
-        mcutils.INSTALL_APP = false;
-        mcutils.HIGHLIGHT = true;
-        mcutils.APP_IDENTIFIER = "com.Advantage.aShopping";
+        mcUtils = new LabDevice();
+        mcUtils.setInstallApp(false);
+        mcUtils.setHighlight(true);
+        mcUtils.setAppIdentifier("com.Advantage.aShopping");
+        mcUtils.setAppVersion("1.1.4");
     }
 
     @AfterMethod
@@ -49,35 +51,36 @@ public class LeanFtTest extends UnitTestClassBase {
             //description.setName("SM-N910C");
             //description.setModel("Sony");
 
-            mcutils.lockDevice(description, DeviceSource.MOBILE_CENTER);
+            mcUtils.lockDevice(description, LabDevice.LabType.SRF);
             //device = MobileLab.lockDevice(description);
 
-            mcutils.setApp();
-            device = mcutils.device;
+            mcUtils.setApp();
+            device = mcUtils.getDevice();
 
             currentDevice = "Allocated device: \"" + device.getName() + "\" (" + device.getId() + "), Model :"
                     + device.getModel() + ", OS: " + device.getOSType() + " version: " + device.getOSVersion()
                     + ", manufacturer: " + device.getManufacturer();
+            
+            Logging.logMessage (currentDevice + ". App in use: \"" + mcUtils.getApp().getName() + "\" v" +
+                    mcUtils.getAppVersion(), Logging.LOG_LEVEL.INFO);
 
-            mcutils.logMessages (currentDevice + ". App in use: \"" + mcutils.app.getName() + "\" v" + mcutils.app.getVersion(), LOG_LEVEL.INFO);
-
-            if (mcutils.INSTALL_APP)
-                mcutils.app.install();
+            if (mcUtils.isInstallApp())
+                mcUtils.getApp().install();
             else
-                mcutils.app.restart();
+                mcUtils.getApp().restart();
 
-            appModel = new AppModelAOS_Android(mcutils.device);
+            appModel = new AppModelAOS_Android(device);
 
             openMenu();
             // log in/out management
-            mcutils.logMessages("Check if I'm logged, if not, sign me in", LOG_LEVEL.INFO);
+            Logging.logMessage("Check if I'm logged, if not, sign me in", Logging.LOG_LEVEL.INFO);
             if (appModel.AdvantageShoppingApplication().SIGNOUTLabel().exists(5)) {     // is anyone signed in?
                 if (appModel.AdvantageShoppingApplication().LoggedInUserNameLabel().getText().compareTo(names[counter]) != 0) {    // is current user signed in?
                     signOut();
                     signIn();
                 }
                 else {      // need to close the menu to continue with the script
-                    if (mcutils.HIGHLIGHT)
+                    if (mcUtils.isHighlight())
                         appModel.AdvantageShoppingApplication().HOMELabel().highlight();
                     appModel.AdvantageShoppingApplication().HOMELabel().tap();
                 }
@@ -86,63 +89,63 @@ public class LeanFtTest extends UnitTestClassBase {
                 signIn();
 
             // select Tablets
-            mcutils.logMessages("Select tablets", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Select tablets", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().TABLETSLabel().highlight();
             appModel.AdvantageShoppingApplication().TABLETSLabel().tap();
 
             windowSync(2000);
             // pick a tablet
-            mcutils.logMessages("Pick a tablet", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Pick a tablet", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().ImageViewProductUiObject().highlight();
             appModel.AdvantageShoppingApplication().ImageViewProductUiObject().tap();
 
             // add to cart
-            mcutils.logMessages("Click 'Add To Cart'", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Click 'Add To Cart'", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().ADDTOCARTButton().highlight();
             appModel.AdvantageShoppingApplication().ADDTOCARTButton().tap();
 
             windowSync(2000);
             // go to cart
-            mcutils.logMessages("Click cart icon", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Click cart icon", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().CartAccessUiObject().highlight();
             appModel.AdvantageShoppingApplication().CartAccessUiObject().tap();
 
             // check out
-            mcutils.logMessages("Click 'Checkout' Pay $...", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Click 'Checkout' Pay $...", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().CHECKOUTPAYButton().highlight();
             appModel.AdvantageShoppingApplication().CHECKOUTPAYButton().tap();
 
             windowSync(5000);
 
             // Pay
-            mcutils.logMessages("Click 'Pay Now'", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Click 'Pay Now'", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().PAYNOWButton().highlight();
             appModel.AdvantageShoppingApplication().PAYNOWButton().tap();
 
-            mcutils.logMessages("Click the 'X' in Order Payment dialog", LOG_LEVEL.INFO);
-            if (mcutils.HIGHLIGHT)
+            Logging.logMessage("Click the 'X' in Order Payment dialog", Logging.LOG_LEVEL.INFO);
+            if (mcUtils.isHighlight())
                 appModel.AdvantageShoppingApplication().ImageViewCloseDialogUiObject().highlight();
             appModel.AdvantageShoppingApplication().ImageViewCloseDialogUiObject().tap();
 
             signOut();
 
-            mcutils.logMessages("======================= Test completed successfully =======================", LOG_LEVEL.INFO);
+            Logging.logMessage("======================= Test completed successfully =======================", Logging.LOG_LEVEL.INFO);
 
         } catch (InterruptedException iex) {
-            mcutils.logMessages("Interrupted exception: " + iex.getMessage() + "\nCurrent device: " + currentDevice, LOG_LEVEL.ERROR);
+            Logging.logMessage("Interrupted exception: " + iex.getMessage() + "\nCurrent device: " + currentDevice, Logging.LOG_LEVEL.ERROR);
         } catch (GeneralLeanFtException err) {
-            mcutils.logMessages("LeanFT/UFTPro exception: " + err.getMessage() + "\nCurrent device: " + currentDevice, LOG_LEVEL.ERROR);
+            Logging.logMessage("LeanFT/UFTPro exception: " + err.getMessage() + "\nCurrent device: " + currentDevice, Logging.LOG_LEVEL.ERROR);
             //Assert.fail("(assertion)");
         } catch (Exception ex) {
-            mcutils.logMessages ("General error: " + ex.getMessage() + "\nCurrent device: " + currentDevice, LOG_LEVEL.ERROR);
+            Logging.logMessage ("General error: " + ex.getMessage() + "\nCurrent device: " + currentDevice, Logging.LOG_LEVEL.ERROR);
         } finally {
-            mcutils.logMessages ("Exit test() \nCurrent device: " + currentDevice, LOG_LEVEL.INFO);
+            Logging.logMessage ("Exit test() \nCurrent device: " + currentDevice, Logging.LOG_LEVEL.INFO);
         }
     }
 
@@ -155,49 +158,49 @@ public class LeanFtTest extends UnitTestClassBase {
     }
 
     private void signOut() throws GeneralLeanFtException {
-        mcutils.logMessages("Enter signing out method", LOG_LEVEL.INFO);
+        Logging.logMessage("Enter signing out method", Logging.LOG_LEVEL.INFO);
         openMenu();
         appModel.AdvantageShoppingApplication().SIGNOUTLabel().tap();
         appModel.AdvantageShoppingApplication().YESButton().tap();
-        mcutils.logMessages("Exit signing out method", LOG_LEVEL.INFO);
+        Logging.logMessage("Exit signing out method", Logging.LOG_LEVEL.INFO);
     }
 
     private void signIn() throws GeneralLeanFtException {
-        mcutils.logMessages ("Enter signIn() method", LOG_LEVEL.INFO);
+        Logging.logMessage ("Enter signIn() method", Logging.LOG_LEVEL.INFO);
 
         // open menu
         openMenu();
 
         // start Login
-        mcutils.logMessages("Start logging in", LOG_LEVEL.INFO);
-        if (mcutils.HIGHLIGHT)
+        Logging.logMessage("Start logging in", Logging.LOG_LEVEL.INFO);
+        if (mcUtils.isHighlight())
             appModel.AdvantageShoppingApplication().LOGINLabel().highlight();
         appModel.AdvantageShoppingApplication().LOGINLabel().tap();
 
         // user name
-        mcutils.logMessages("User name is as " + names[counter], LOG_LEVEL.INFO);
-        if (mcutils.HIGHLIGHT)
+        Logging.logMessage("User name is as " + names[counter], Logging.LOG_LEVEL.INFO);
+        if (mcUtils.isHighlight())
             appModel.AdvantageShoppingApplication().USERNAMELabel().highlight();
         appModel.AdvantageShoppingApplication().MobileEditEditUserNameField().setText(names[counter]);
 
         // password
-        mcutils.logMessages("Encrypted password is " + passwords[counter], LOG_LEVEL.INFO);
-        if (mcutils.HIGHLIGHT)
+        Logging.logMessage("Encrypted password is " + passwords[counter], Logging.LOG_LEVEL.INFO);
+        if (mcUtils.isHighlight())
             appModel.AdvantageShoppingApplication().PASSWORDLabel().highlight();
         appModel.AdvantageShoppingApplication().MobileEditEditPasswordField().setSecure(passwords[counter++]);
 
         // log-in
-        mcutils.logMessages("Click 'LOGIN'", LOG_LEVEL.INFO);
-        if (mcutils.HIGHLIGHT)
+        Logging.logMessage("Click 'LOGIN'", Logging.LOG_LEVEL.INFO);
+        if (mcUtils.isHighlight())
             appModel.AdvantageShoppingApplication().LOGINButton().highlight();
         appModel.AdvantageShoppingApplication().LOGINButton().tap();
 
-        mcutils.logMessages ("Exit signIn() method", LOG_LEVEL.INFO);
+        Logging.logMessage ("Exit signIn() method", Logging.LOG_LEVEL.INFO);
     }
 
     private void openMenu() throws GeneralLeanFtException{
-        mcutils.logMessages("Open menu", LOG_LEVEL.INFO);
-        if (mcutils.HIGHLIGHT)
+        Logging.logMessage("Open menu", Logging.LOG_LEVEL.INFO);
+        if (mcUtils.isHighlight())
             appModel.AdvantageShoppingApplication().ImageViewHamburger().highlight();
         appModel.AdvantageShoppingApplication().ImageViewHamburger().tap();
     }
